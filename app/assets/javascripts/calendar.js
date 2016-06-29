33bdd214e8ec27d3774dc382062bce48a82ed60b
thanhtt <tran.tien.thanh@framgia.com>
2016-06-29 15:02:38 +0700

    notify desktop when delete event

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -897,6 +897,27 @@ $(document).on('page:change', function() {
     }
   });
 
+  $('#my-input').autocomplete({
+      source: aCleanData,
+      minLength: 2,
+      search: function(oEvent, oUi) {
+          // get current input value
+          var sValue = $(oEvent.target).val();
+          // init new search array
+          var aSearch = [];
+          // for each element in the main array ...
+          $(aCleanData).each(function(iIndex, sElement) {
+              // ... if element starts with input value ...
+              if (sElement.substr(0, sValue.length) == sValue) {
+                  // ... add element
+                  aSearch.push(sElement);
+              }
+          });
+          // change search array
+          $(this).autocomplete('option', 'source', aSearch);
+      }
+  });
+
   $('#load-attendee').autocomplete({
     source: '/api/search',
     create: function(){
